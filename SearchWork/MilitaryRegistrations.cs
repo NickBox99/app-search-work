3c4004414a5395dc97b0c7d22bd1f98e1b0e605f
Dmitry <nickbox99@inbox.ru>
2023-05-20 23:21:34 +0400

    feat: add methods

--- SearchWork/MilitaryRegistrations.cs
+++ SearchWork/MilitaryRegistrations.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SearchWork.Database;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,56 @@ namespace SearchWork
         public MilitaryRegistrations()
         {
             InitializeComponent();
+            GendersSql.renderTable(dataGridView1);
+        }
+
+        private int getRowSelectedId()
+        {
+            var value = dataGridView1.CurrentRow.Cells[0].Value;
+
+            if (value != null)
+            {
+                return Convert.ToInt32(value);
+            }
+            else
+            {
+                MessageBox.Show("Необходимо выбрать строку");
+            }
+
+            return -1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new MilitaryRegistrationAdd().Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var id = getRowSelectedId();
+
+            if (id != -1)
+            {
+                new MilitaryRegistrationEdit(id).Show();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var id = getRowSelectedId();
+
+            if (id != -1)
+            {
+                GendersSql.delete(id);
+                GendersSql.renderTable(dataGridView1);
+                MessageBox.Show("Удалено");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            GendersSql.renderTable(dataGridView1);
+            MessageBox.Show("Обновлено");
         }
     }
 }
