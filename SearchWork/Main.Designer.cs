f1002fa1b18f855f2912f021ad95c505868f9bec
Dmitry <nickbox99@inbox.ru>
2023-05-20 20:07:56 +0400

    feat: add interface

--- SearchWork/Main.Designer.cs
+++ SearchWork/Main.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Main";
+            this.Name = "Main";
+            this.Text = "Меню";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
