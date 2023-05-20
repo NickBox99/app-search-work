ce7a088ed1bf1aa026ed606dbdff9317307f35ca
Dmitry <nickbox99@inbox.ru>
2023-05-20 23:17:46 +0400

    feat: create MilitaryRegistrationsSql class

---
+++ SearchWork/Database/MilitaryRegistrationsSql.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SearchWork.Database
+{
+    static class MilitaryRegistrationsSql
+    {
+        public static void renderTable(DataGridView dataGridView)
+        {
+            Database.renderTable("military_registrations", dataGridView);
+        }
+
+        private static Dictionary<string, object> getDataDictionary(string name)
+        {
+            return new Dictionary<string, object>() {
+                { "name", name }
+            };
+        }
+
+        public static void add(string name)
+        {
+            Database.add("military_registrations", getDataDictionary(name));
+        }
+
+        public static void update(int id, string name)
+        {
+            Database.update("military_registrations", id, getDataDictionary(name));
+        }
+
+        public static void delete(int id)
+        {
+            Database.delete("military_registrations", id);
+        }
+
+        public static Dictionary<string, object> get(int id)
+        {
+            return Database.get("military_registrations", id);
+        }
+    }
+}
