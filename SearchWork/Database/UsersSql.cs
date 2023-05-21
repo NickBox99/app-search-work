c10a305d943c0e7b273cc196304a6188068ff274
Dmitry <nickbox99@inbox.ru>
2023-05-21 15:58:38 +0400

    feat: add methods

--- SearchWork/Database/UsersSql.cs
+++ SearchWork/Database/UsersSql.cs
@@ -7,6 +7,7 @@ using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using System.Xml.Linq;
 using Npgsql;
 using System.Data;
+using System.Xml;
 
 namespace SearchWork.Database
 {
@@ -46,9 +47,34 @@ namespace SearchWork.Database
             Database.authLogin = login;
         }
 
-        static public NpgsqlDataReader get(string login)
+        static public void update(int id, string login, string fio, DateTime dateOfBirth, string address, string phone, string experience, string resume, string education, decimal salary, string position, int genderId, int militaryRegistrationId, int typeOfApplicantId)
         {
-            return Database.sendRequest($"select * from users_auth WHERE login = '{login}'");
+            var userData = getDataDictionary(fio, dateOfBirth, address, phone, experience, resume, education, salary, position, genderId, militaryRegistrationId, typeOfApplicantId);
+
+            Database.update("users", id, userData);
+        }
+
+        static public Dictionary<string, object> get(string login)
+        {
+            var reader = Database.sendRequest($"select ua.login, ua.password, u.fio, u.address, u.date_of_birth, u.id, u.phone, u.experience, u.resume, u.education, u.salary, u.position, u.gender_id, u.military_registration_id, u.type_of_applicant_id FROM users_auth ua JOIN users u ON ua.user_id = u.id WHERE ua.login = '{login}'");
+
+            Dictionary<string, object> data = new Dictionary<string, object>();
+
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    string columnName = reader.GetName(i);
+                    object value = reader.GetValue(i);
+
+                    if (value != null && value != DBNull.Value)
+                    {
+                        data.Add(columnName, value);
+                    }
+                }
+            }
+
+            return data;
         }
 
         static public string init(string login, string password) {
@@ -56,9 +82,9 @@ namespace SearchWork.Database
 
             var user = get(login);
 
-            if (user.Read())
+            if (user.Count != 0)
             {
-                if (user.GetString(3) != password)
+                if ((string)user["password"] != password)
                 {
                     result = "Пароль неверный";
                 }
