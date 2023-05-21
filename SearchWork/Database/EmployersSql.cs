795b8ed8ade17ef284f07c493c8d2af3e621e5d4
Dmitry <nickbox99@inbox.ru>
2023-05-21 20:05:31 +0400

    chore: clear methods

--- SearchWork/Database/EmployersSql.cs
+++ SearchWork/Database/EmployersSql.cs
@@ -7,15 +7,12 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using System.Xml.Linq;
+using System.Xml;
 
 namespace SearchWork.Database
 {
     static class EmployersSql
     {
-        public static void renderTable(DataGridView dataGridView)
-        {
-            Database.renderTable("employers", dataGridView);
-        }
         private static Dictionary<string, object> getDataDictionary(string name, string inn, string status, string address, string phone, string additionalInfo)
         {
             return new Dictionary<string, object>() {
@@ -26,21 +23,6 @@ namespace SearchWork.Database
                 { "phone", phone },
                 { "additional_info", additionalInfo }
             };
-        }
-
-        public static void add(string name, string inn, string status, string address, string phone, string additionalInfo)
-        {
-            Database.add("employers", getDataDictionary(name, inn, status, address, phone, additionalInfo));
-        }
-
-        public static void delete(int id)
-        {
-            Database.delete("employers", id);
-        }
-
-        public static Dictionary<string, object> get(int id)
-        {
-            return Database.get("employers", id);
         }
 
         public static void update(int id, string name, string inn, string status, string address, string phone, string additionalInfo)
