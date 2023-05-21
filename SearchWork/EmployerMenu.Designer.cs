1326b1ef8045fd615d9a4945ed40bfba5ff819c6
Dmitry <nickbox99@inbox.ru>
2023-05-21 20:07:34 +0400

    feat: add methods

--- SearchWork/EmployerMenu.Designer.cs
+++ SearchWork/EmployerMenu.Designer.cs
@@ -30,12 +30,21 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EmployerMenu));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.salary = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.min_age = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.max_age = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.requirements = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.work_schedule = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.vacancy_status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.additional_info = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,53 +57,49 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.id,
-            this.name});
+            this.salary,
+            this.min_age,
+            this.max_age,
+            this.requirements,
+            this.work_schedule,
+            this.vacancy_status,
+            this.additional_info});
             this.dataGridView1.Location = new System.Drawing.Point(12, 43);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersVisible = false;
-            this.dataGridView1.Size = new System.Drawing.Size(589, 319);
+            this.dataGridView1.Size = new System.Drawing.Size(936, 360);
             this.dataGridView1.TabIndex = 16;
-            // 
-            // id
-            // 
-            this.id.HeaderText = "Id";
-            this.id.Name = "id";
-            this.id.ReadOnly = true;
-            // 
-            // name
-            // 
-            this.name.HeaderText = "Наименование";
-            this.name.Name = "name";
-            this.name.ReadOnly = true;
             // 
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.Ivory;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button3.Location = new System.Drawing.Point(669, 113);
+            this.button3.Location = new System.Drawing.Point(954, 91);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(245, 42);
             this.button3.TabIndex = 15;
-            this.button3.Text = "Откликнуться";
+            this.button3.Text = "Добавить";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.Ivory;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.Location = new System.Drawing.Point(669, 209);
+            this.button2.Location = new System.Drawing.Point(954, 283);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(245, 42);
             this.button2.TabIndex = 14;
             this.button2.Text = "Обновить список";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.Ivory;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(669, 161);
+            this.button1.Location = new System.Drawing.Point(954, 43);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(245, 42);
             this.button1.TabIndex = 13;
@@ -106,18 +111,105 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(247, 17);
+            this.label1.Location = new System.Drawing.Point(438, 12);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(81, 20);
             this.label1.TabIndex = 17;
             this.label1.Text = "Вакансии";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Ivory;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(954, 187);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(245, 42);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "Удалить";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Ivory;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(954, 139);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(245, 42);
+            this.button5.TabIndex = 19;
+            this.button5.Text = "Редактировать";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Ivory;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.Location = new System.Drawing.Point(954, 235);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(245, 42);
+            this.button6.TabIndex = 20;
+            this.button6.Text = "Посмотреть отклики";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // id
+            // 
+            this.id.HeaderText = "Id";
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            // 
+            // salary
+            // 
+            this.salary.HeaderText = "Зарплата";
+            this.salary.Name = "salary";
+            this.salary.ReadOnly = true;
+            // 
+            // min_age
+            // 
+            this.min_age.HeaderText = "Возраст (мин)";
+            this.min_age.Name = "min_age";
+            this.min_age.ReadOnly = true;
+            // 
+            // max_age
+            // 
+            this.max_age.HeaderText = "Возраст (макс)";
+            this.max_age.Name = "max_age";
+            this.max_age.ReadOnly = true;
+            // 
+            // requirements
+            // 
+            this.requirements.HeaderText = "Требования";
+            this.requirements.Name = "requirements";
+            this.requirements.ReadOnly = true;
+            // 
+            // work_schedule
+            // 
+            this.work_schedule.HeaderText = "График работы";
+            this.work_schedule.Name = "work_schedule";
+            this.work_schedule.ReadOnly = true;
+            // 
+            // vacancy_status
+            // 
+            this.vacancy_status.HeaderText = "Статус вакансии";
+            this.vacancy_status.Name = "vacancy_status";
+            this.vacancy_status.ReadOnly = true;
+            // 
+            // additional_info
+            // 
+            this.additional_info.HeaderText = "Доп. информация";
+            this.additional_info.Name = "additional_info";
+            this.additional_info.ReadOnly = true;
             // 
             // EmployerMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Info;
-            this.ClientSize = new System.Drawing.Size(1211, 450);
+            this.ClientSize = new System.Drawing.Size(1211, 416);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -137,11 +229,20 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn id;
-        private System.Windows.Forms.DataGridViewTextBoxColumn name;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn salary;
+        private System.Windows.Forms.DataGridViewTextBoxColumn min_age;
+        private System.Windows.Forms.DataGridViewTextBoxColumn max_age;
+        private System.Windows.Forms.DataGridViewTextBoxColumn requirements;
+        private System.Windows.Forms.DataGridViewTextBoxColumn work_schedule;
+        private System.Windows.Forms.DataGridViewTextBoxColumn vacancy_status;
+        private System.Windows.Forms.DataGridViewTextBoxColumn additional_info;
     }
 }
