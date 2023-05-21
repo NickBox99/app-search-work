8bf7f893fe2b126cba8f2743bcbe52748b159132
Dmitry <nickbox99@inbox.ru>
2023-05-21 15:59:31 +0400

    -

--- SearchWork/UserMenu.Designer.cs
+++ SearchWork/UserMenu.Designer.cs
@@ -60,6 +60,7 @@
             this.button1.TabIndex = 8;
             this.button1.Text = "Профиль";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button3
             // 
