ca54ef56b23f00ce2fe01ea1c9e0f0697a9015f2
Dmitry <nickbox99@inbox.ru>
2023-05-21 15:59:21 +0400

    feat: add open form method

--- SearchWork/UserMenu.cs
+++ SearchWork/UserMenu.cs
@@ -16,5 +16,10 @@ namespace SearchWork
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new UserProfile().Show();
+        }
     }
 }
