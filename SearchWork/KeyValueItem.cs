74e21817c2dc15461a64fcb6588eb08e5d435567
Dmitry <nickbox99@inbox.ru>
2023-05-21 14:24:50 +0400

    feat: add KeyValueItem class

---
+++ SearchWork/KeyValueItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchWork
+{
+    public class KeyValueItem
+    {
+        public int Key { get; set; }
+        public string Value { get; set; }
+
+        public KeyValueItem(int key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+    }
+}
